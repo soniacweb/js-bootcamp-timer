05ed19ad422781ce45e41743970f0af217bc1c19
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2021-01-01 22:58:35 +0000

    on methods complete

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 class Timer {
-  constructor(duration, startb, pause) {
+  constructor(duration, startb, pause, callbacks) {
     this.duration = duration
     this.startb = startb
     this.pauseb = pause
+    if (callbacks) {
+      this.onStart = callbacks.onStart
+      this.onTick = callbacks.onTick
+      this.onComplete = callbacks.onComplete 
 
+
+    }
     this.startb.addEventListener('click', this.start)
     this.pauseb.addEventListener('click', this.pause)
 
   }
   start = () => {
+    if (this.onStart) {
+      this.onStart()
+    }
     this.tick()
-    this.timer = setInterval(this.tick, 1000)
+    setInterval(this.tick, 1000)
   }
 
-  tick() {
-    const timeRemaining = parseFloat(this.duration.value)
-    console.log(timeRemaining)
-    this.duration.value = timeRemaining -1
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause()
+      if (this.onComplete) {
+        this.onComplete()
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 1
+      if (this.onTick) {
+        this.onTick()
+      }
+    }
+  }
+
+  get timeRemaining() {
+    return parseFloat(this.duration.value)
+  }
+
+  set timeRemaining(time) {
+    this.duration.value = time
   }
 
   pause = () => {
@@ -28,4 +53,16 @@ const duration = document.querySelector('#duration')
 const startb = document.querySelector('#start')
 const pause = document.querySelector('#pause')
 
-const timer = new Timer(duration, startb, pause)
+const timer = new Timer(duration, startb, pause, {
+  onStart() {
+    console.log('started')
+  },
+  onTick() {
+    console.log('ticked')
+
+  }, 
+  onComplete() {
+    console.log('completed')
+
+  }
+})
