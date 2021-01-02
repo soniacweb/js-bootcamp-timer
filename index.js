4c07678c5fbbf9772825b1ab1ffef44e4655aaa9
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2021-01-02 20:51:42 +0000

    font awesome link

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const pause = document.querySelector('#pause')
 const circle = document.querySelector('circle')
 const perimeter = circle.getAttribute('r') * 2 * Math.PI
 circle.setAttribute('stroke-dasharray', perimeter)
-let currentOffset = 0
+// let currentOffset = 0
 
 //creating a new instance of the object
 const timer = new Timer(duration, startb, pause, {
-  onStart() {
-    console.log('started')
+  onStart(totalDuration) {
+    duration1 = totalDuration 
   },
-  onTick() {
-    circle.setAttribute('stroke-dashoffset', currentOffset)
-    currentOffset = currentOffset - 50
+  onTick(timeRemaining) {
+    circle.setAttribute('stroke-dashoffset',
+      (perimeter * timeRemaining) / duration1 - perimeter
+    )
+    
   }, 
   onComplete() {
     console.log('completed')
--- timer.js
+++ timer.js
@@ -15,10 +15,10 @@ class Timer {
 
   start = () => {
     if (this.onStart) {
-      this.onStart()
+      this.onStart(this.timeRemaining)
     }
     this.tick()
-    this.interval = setInterval(this.tick, 1000)
+    this.interval = setInterval(this.tick, 50)
   }
 
   pause = () => {
@@ -32,9 +32,9 @@ class Timer {
         this.onComplete()
       }
     } else {
-      this.timeRemaining = this.timeRemaining - 1
+      this.timeRemaining = this.timeRemaining - 0.05
       if (this.onTick) {
-        this.onTick()
+        this.onTick(this.timeRemaining)
       }
     }
   }
@@ -44,7 +44,7 @@ class Timer {
   }
 //setting the variable
   set timeRemaining(time) {
-    this.duration.value = time
+    this.duration.value = time.toFixed(2)
   }
  
 }
